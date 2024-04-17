acf42c2348916abdd99b285b2a632f19ea5d6241
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2024-04-17 13:56:03 -0400

    Update source files

--- Bookstore/src/main/Book.java
+++ Bookstore/src/main/Book.java
@@ -1,46 +1,32 @@
 package main;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
-
-public class Main {
-
-	public static void main(String args[]) {
-		
+public class Book {
+	
+	public Book() {
 		try {
 			readCsv();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 	
 	public static void readCsv() throws IOException {
 		// Title, Author, ISBN, Date Published, Publisher, Pages, Genre, Description, Format, Price
-		String[] = new String[]
 		File file = new File("Files/BookstoreList.csv");
 	    System.out.println(file);
 	    
 		FileReader fileReader = new FileReader(file);
-		System.out.println(fileReader);
 	    BufferedReader bufferedReader = new BufferedReader(fileReader);
-	    System.out.println(bufferedReader);
 	    String line;
 	    String[] tokenizedLine;
 
 	    while ((line = bufferedReader.readLine()) != null) {
-	    	System.out.println(line + "\n");
-	    	tokenizedLine = line.split(",");
-	        // tokenizedLine = parse(line);
-	        
+	        tokenizedLine = parse(line);
 	        System.out.println(tokenizedLine);
 	    }
 	}
@@ -48,5 +34,6 @@ public class Main {
 	private static String[] parse(String line) { // use split or Scanner
         return line.split(",");
     }
-    
+	
+	
 }
